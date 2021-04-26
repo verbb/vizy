d74b23de5b159a9365b0c536d8c9823929e8d209
Josh Crawford <joshua@sgroup.com.au>
2021-04-26 14:24:40 +1000

    Fix heading tags

--- src/nodes/Heading.php
+++ src/nodes/Heading.php
@@ -3,6 +3,8 @@ namespace verbb\vizy\nodes;
 
 use verbb\vizy\base\Node;
 
+use craft\helpers\ArrayHelper;
+
 class Heading extends Node
 {
     // Properties
@@ -16,7 +18,21 @@ class Heading extends Node
 
     public function getTag()
     {
-        return "h{$this->attrs['level']}";
+        // Don't include certain attributes in rendering
+        $level = ArrayHelper::remove($this->attrs, 'level');
+        $align = ArrayHelper::remove($this->attrs, 'textAlign');
+
+        // Don't overwrite it for the closing tag
+        if (!$this->tagName) {
+            $this->tagName = "h{$level}";
+        }
+
+        // Add instead as a class, `text-left`, `text-right`, etc.
+        if ($align && $align !== 'start') {
+            $this->attrs['class'] = trim(($this->attrs['class'] ?? '') . ' text-' . $align);
+        }
+
+        return parent::getTag();
     }
 
 }
