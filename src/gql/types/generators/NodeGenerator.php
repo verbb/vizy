7b3c95ae989f3bdca18bcf20e787fb448b3502f5
andris-sevcekno <andris@pixelandtonic.com>
2021-05-25 22:20:48 +0300

    Rework the Node Generator.

--- src/gql/types/generators/NodeGenerator.php
+++ src/gql/types/generators/NodeGenerator.php
@@ -1,20 +1,11 @@
 <?php
 namespace verbb\vizy\gql\types\generators;
 
-use verbb\vizy\Vizy;
-use verbb\vizy\gql\interfaces\NodeInterface;
-use verbb\vizy\gql\interfaces\VizyBlockInterface;
-use verbb\vizy\gql\types\NodeType;
-use verbb\vizy\gql\types\VizyBlockType;
-
-use Craft;
 use craft\gql\base\GeneratorInterface;
 use craft\gql\GqlEntityRegistry;
-use craft\gql\TypeLoader;
-use craft\gql\TypeManager;
-use craft\helpers\Gql as GqlHelper;
-
-use GraphQL\Type\Definition\Type;
+use verbb\vizy\gql\interfaces\NodeInterface;
+use verbb\vizy\gql\types\NodeType;
+use verbb\vizy\Vizy;
 
 class NodeGenerator implements GeneratorInterface
 {
@@ -26,45 +17,31 @@ class NodeGenerator implements GeneratorInterface
         $nodeClasses = Vizy::$plugin->getNodes()->getRegisteredNodes();
 
         $gqlTypes = [];
+        $interfaceFields = NodeInterface::getFieldDefinitions();
 
         foreach ($nodeClasses as $nodeClass) {
             $node = new $nodeClass;
             $node->setField($field);
 
             $typeName = $node->getGqlTypeName();
-            $nodeGqlDefinitions = $node->getContentGqlType($node);
 
-            // Handle Vizy Block nodes differently
-            if ($node->type === 'vizyBlock') {
-                $typeArray = VizyBlockTypeGenerator::generateTypes($field);
-                $resolver = function($value) {
-                    return $value->getGqlTypeName();
-                };
+            if (!($entity = GqlEntityRegistry::getEntity($typeName))) {
+                $contentType = $node->getContentGqlType($field);
 
-                $contentFieldGqlTypes = [
-                    'name' => $field->handle,
-                    'type' => Type::listOf(GqlHelper::getUnionType($field->handle . '_VizyField', $typeArray, $resolver)),
-                    'resolve' => VizyBlockResolver::class . '::resolve',
-                ];
+                // Override content field with the nodes content type.
+                $nodeFields = array_merge($interfaceFields, ['content' => $contentType]);
 
-                $nodeFields = TypeManager::prepareFieldDefinitions(array_merge(VizyBlockInterface::getFieldDefinitions(), $contentFieldGqlTypes), $typeName);
-
-                $gqlTypes[$typeName] = GqlEntityRegistry::getEntity($typeName) ?: GqlEntityRegistry::createEntity($typeName, new VizyBlockType([
+                $entity = new NodeType([
                     'name' => $typeName,
                     'fields' => function() use ($nodeFields) {
                         return $nodeFields;
                     }
-                ]));
-            } else {
-                $nodeFields = TypeManager::prepareFieldDefinitions(array_merge(NodeInterface::getFieldDefinitions(), $nodeGqlDefinitions), $typeName);
+                ]);
 
-                $gqlTypes[$typeName] = GqlEntityRegistry::getEntity($typeName) ?: GqlEntityRegistry::createEntity($typeName, new NodeType([
-                    'name' => $typeName,
-                    'fields' => function() use ($nodeFields) {
-                        return $nodeFields;
-                    }
-                ]));
+                $entity = GqlEntityRegistry::getEntity($typeName) ?: GqlEntityRegistry::createEntity($typeName, $entity);
             }
+
+            $gqlTypes[$entity->name] = $entity;
         }
 
         return $gqlTypes;
