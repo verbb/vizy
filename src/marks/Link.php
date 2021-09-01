0451201ba91de0e990cff41c6037ed3348b2b834
Josh Crawford <joshua@sgroup.com.au>
2021-09-01 20:36:46 +1000

    Fix links containing ref tags not being parsed properly

--- src/marks/Link.php
+++ src/marks/Link.php
@@ -3,6 +3,8 @@ namespace verbb\vizy\marks;
 
 use verbb\vizy\base\Mark;
 
+use Craft;
+
 class Link extends Mark
 {
     // Properties
@@ -22,6 +24,33 @@ class Link extends Mark
                 $this->attrs['rel'] = 'noopener noreferrer nofollow';
             }
         }
+
+        // Parse the link URL for ref tags
+        $href = $this->attrs['href'] ?? '';
+
+        if ($href) {
+            $siteId = $this->element->siteId ?? null;
+
+            $this->attrs['href'] = $this->_parseRefTags($href, $siteId);
+        }
+
         return parent::getTag();
+    }
+
+
+    // Private Methods
+    // =========================================================================
+
+    private function _parseRefTags($value, $siteId)
+    {
+        $split = explode('#', $value);
+
+        if (isset($split[0]) && isset($split[1])) {
+            $parsedUrl = '{' . $split[1] . ':url||' . $split[0] . '}';
+
+            return Craft::$app->getElements()->parseRefs($parsedUrl, $siteId);
+        }
+
+        return $value;
     }
 }
