5485464d5b2bbb5002caf826f298b7745a8c45d5
Josh Crawford <joshua@sgroup.com.au>
2022-03-07 13:25:45 +1100

    Update field layout handling

--- src/controllers/FieldController.php
+++ src/controllers/FieldController.php
@@ -2,6 +2,7 @@
 namespace verbb\vizy\controllers;
 
 use verbb\vizy\Vizy;
+use verbb\vizy\helpers\Fields;
 
 use Craft;
 use craft\fieldlayoutelements\CustomField;
@@ -27,63 +28,17 @@ class FieldController extends Controller
         $fieldLayoutUid = $request->getParam('layoutUid');
         $blockTypeId = $request->getParam('blockTypeId');
         $fieldIds = $request->getParam('fieldIds');
-        $elementPlacements = $request->getParam('elementPlacements', []);
-        $elementConfigs = $request->getParam('elementConfigs', []);
+        $layoutConfig = $request->getParam('layout', []);
 
-        $fieldLayout = null;
+        $fieldLayout = new FieldLayout();
 
         if ($fieldLayoutUid) {
             $fieldLayout = Vizy::$plugin->getService()->getFieldLayoutByUid($fieldLayoutUid);
         }
 
-        if (!$fieldLayout) {
-            $fieldLayout = new FieldLayout();
-        }
-
         // Prep the field layout from post - we could be editing an unsaved field layout
-        if ($elementPlacements && $elementConfigs) {
-            $tabs = [];
-            $fields = [];
-            $tabSortOrder = 0;
-
-            foreach ($elementPlacements as $tabName => $elementKeys) {
-                $tab = $tabs[] = new FieldLayoutTab();
-                $tab->name = urldecode($tabName);
-                $tab->sortOrder = ++$tabSortOrder;
-                $tab->elements = [];
-
-                if (!is_array($elementKeys)) {
-                    continue;
-                }
-
-                foreach ($elementKeys as $i => $elementKey) {
-                    $elementConfig = Json::decode($elementConfigs[$elementKey]);
-
-                    try {
-                        $element = Craft::$app->getFields()->createLayoutElement($elementConfig);
-                    } catch (InvalidArgumentException $e) {
-                        throw new BadRequestHttpException($e->getMessage(), 0, $e);
-                    }
-
-                    $tab->elements[] = $element;
-
-                    if ($element instanceof CustomField) {
-                        $fieldUid = $element->getFieldUid();
-                        $field = Craft::$app->getFields()->getFieldByUid($fieldUid);
-
-                        if (!$field) {
-                            throw new BadRequestHttpException("Invalid field UUID: $fieldUid");
-                        }
-
-                        $field->required = (bool)($elementConfig['required'] ?? false);
-                        $field->sortOrder = ($i + 1);
-                        $fields[] = $field;
-                    }
-                }
-            }
-
-            $fieldLayout->setTabs($tabs);
-            $fieldLayout->setFields($fields);
+        if ($layoutConfig) {
+            $fieldLayout = FieldLayout::createFromConfig(Json::decode($layoutConfig));
         }
 
         // Fetch the available custom fields for the layout - we want to add some exceptions
@@ -100,22 +55,16 @@ class FieldController extends Controller
             }
         }
 
-        // Ensure to namespace the FLD so it's unique. Important when used in Matrix blocks
-        // as under normal Vizy field circumstances, you edit a one FLD at a time.
-        $originalNamespace = $view->getNamespace();
-        $namespace = $view->namespaceInputName(str_replace('type-', '', $blockTypeId), $originalNamespace);
-        $view->setNamespace($namespace);
-
         // Render the HTML for the FLD to send back to Vue
-        $html = $view->renderTemplate('vizy/field/_includes/fld', [
-            'fieldLayout' => $fieldLayout,
+        $html = Fields::fieldLayoutDesignerHtml($fieldLayout, [
+            // Ensure to namespace the FLD so it's unique. Important when used in Matrix blocks
+            // as under normal Vizy field circumstances, you edit one FLD at a time.
+            'id' => str_replace('type-', '', $blockTypeId) . 'fld' . mt_rand(),
             'availableCustomFields' => $availableCustomFields,
         ]);
 
         $headHtml = $view->getHeadHtml();
         $footHtml = $view->getBodyHtml();
-
-        $view->setNamespace($originalNamespace);
 
         return $this->asJson([
             'html' => $html,
