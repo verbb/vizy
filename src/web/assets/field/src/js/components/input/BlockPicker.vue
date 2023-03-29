10db90eb36572a74f2f51c3560bd942fb6fe4873
Josh Crawford <joshua@sgroup.com.au>
2023-03-29 18:23:42 +1100

    Block picker keyboard escape

--- src/web/assets/field/src/js/components/input/BlockPicker.vue
+++ src/web/assets/field/src/js/components/input/BlockPicker.vue
@@ -6,6 +6,7 @@
             :style="`top: ${menu.top}px`"
             @keydown.left.prevent="moveSelectionLeft"
             @keydown.right.prevent="moveSelectionRight"
+            @keydown.escape.prevent="closePane"
         >
             <button type="button" aria-label="Add block" aria-haspopup="true" aria-expanded="false" class="vui-editor-insert-btn" @click="onClick">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path fill="currentColor" d="M368 224H224V80c0-8.84-7.16-16-16-16h-32c-8.84 0-16 7.16-16 16v144H16c-8.84 0-16 7.16-16 16v32c0 8.84 7.16 16 16 16h144v144c0 8.84 7.16 16 16 16h32c8.84 0 16-7.16 16-16V288h144c8.84 0 16-7.16 16-16v-32c0-8.84-7.16-16-16-16z" /></svg>
@@ -236,6 +237,12 @@ export default {
                 const tr = view.state.tr.setSelection(selection);
 
                 view.dispatch(tr);
+            }
+        },
+
+        closePane() {
+            if (this.tippy) {
+                this.tippy.hide();
             }
         },
     },
