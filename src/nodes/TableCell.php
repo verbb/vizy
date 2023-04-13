6d3b9af28a7e1b3e2154cdddbb989a3f7a0a8cc9
Josh Crawford <joshua@sgroup.com.au>
2023-04-13 14:11:11 +1000

    Fix empty table cells not saving correctly

--- src/nodes/TableCell.php
+++ src/nodes/TableCell.php
@@ -3,6 +3,8 @@ namespace verbb\vizy\nodes;
 
 use verbb\vizy\base\Node;
 
+use craft\base\ElementInterface;
+
 class TableCell extends Node
 {
     // Properties
@@ -41,6 +43,21 @@ class TableCell extends Node
                 'attrs' => $attrs,
             ],
         ];
+    }
+
+    public function serializeValue(ElementInterface $element = null): ?array
+    {
+        $value = parent::serializeValue($element);
+
+        $value['content'] = array_filter(($value['content'] ?? []));
+
+        // Table headers/cells seem to struggle if any empty paragraph is stripped out, which will happen by default
+        // so always ensure at least a paragraph node exists.
+        if (!$value['content']) {
+            $value['content'] = [['type' => 'paragraph']];
+        }
+
+        return $value;
     }
 
 }
--- src/nodes/TableHeader.php
+++ src/nodes/TableHeader.php
@@ -3,6 +3,8 @@ namespace verbb\vizy\nodes;
 
 use verbb\vizy\base\Node;
 
+use craft\base\ElementInterface;
+
 class TableHeader extends Node
 {
     // Properties
@@ -10,5 +12,24 @@ class TableHeader extends Node
 
     public static ?string $type = 'tableHeader';
     public mixed $tagName = 'th';
+
+
+    // Public Methods
+    // =========================================================================
+
+    public function serializeValue(ElementInterface $element = null): ?array
+    {
+        $value = parent::serializeValue($element);
+
+        $value['content'] = array_filter(($value['content'] ?? []));
+
+        // Table headers/cells seem to struggle if any empty paragraph is stripped out, which will happen by default
+        // so always ensure at least a paragraph node exists.
+        if (!$value['content']) {
+            $value['content'] = [['type' => 'paragraph']];
+        }
+
+        return $value;
+    }
 
 }
