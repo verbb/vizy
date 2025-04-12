7da6fb40d743d84aa38551f8724be161c6764d03
Josh Crawford <joshua@sgroup.com.au>
2025-04-13 07:30:52 +1000

    Add Vizy field context to block templates

--- src/nodes/VizyBlock.php
+++ src/nodes/VizyBlock.php
@@ -167,8 +167,10 @@ class VizyBlock extends Node
 
         // Create a fake element with the same fieldtype as our block
         $block = $this->getBlockElement();
+        $field = $this->getField();
 
         $variables = array_merge($this->toArray(), $block->getFieldValues());
+        $variables[$field->handle] = $field;
 
         return $view->renderTemplate($this->_blockType->template, $variables, View::TEMPLATE_MODE_SITE);
     }
