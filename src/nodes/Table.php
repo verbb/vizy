b83916ed7dc952e1a2bc7661ad670763b5eba3e7
Josh Crawford <joshua@sgroup.com.au>
2025-10-31 09:29:53 +1100

    Fix an issue for Table nodes, where attributes set on `table` were also set on `tbody`

--- src/nodes/Table.php
+++ src/nodes/Table.php
@@ -11,4 +11,16 @@ class Table extends Node
     public static ?string $type = 'table';
     public mixed $tagName = ['table', 'tbody'];
 
+
+    public function getTag(): array
+    {
+        // Strip out attributes for `tbody` until we figure out a better way to define them
+        // https://github.com/verbb/vizy/issues/355
+        $tags = parent::getTag();
+        $tags[0]['tag'] = 'table';
+        $tags[1]['tag'] = 'tbody';
+
+        return $tags;
+    }
+
 }
